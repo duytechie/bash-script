68a1c49f26a570051c6f309ae3c19c75efd0ceaa
duytechie <trand2221@gmail.com>
2024-09-17 12:50:56 +0000

    add bash files

---
+++ arithmetic_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+x=4
+y=2
+
+echo $(( x + y ))
+echo $(( x - y ))
+echo $(( x * y ))
+echo $(( x / y ))
---
+++ our_first_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Author: Duy Tran 
+# Date Created: 14Sep2024
+# Last Modified: 14Sep2024
+
+# Description
+# Print texts to the screen.
+
+# Usage
+# our_first_script
+
+echo "This is my first script."
+exit 0
